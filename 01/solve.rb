5035ede72fc6f2a2f2e9506e1e14ccb8522e27b2
David Rueck <drueck@gmail.com>
2017-12-01 14:48:42 -0800

    Day 1

---
+++ 01/solve.rb
@@ -0,0 +1,12 @@
+input = ARGV[0]
+exit if input.nil?
+
+input_array = input.chars
+input_array << input_array[0]
+
+sum = 0
+input_array.each_cons(2) { |pair|
+  sum += pair.first.to_i if pair[0] == pair[1]
+}
+
+puts sum
